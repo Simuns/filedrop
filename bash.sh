1c0897e250b439dff8d10b1ef0674e1cba67b38c
simuns <sih@synack.fo>
2021-04-30 19:30:28 +0100

    kom nú bara

--- bash.sh
+++ bash.sh
@@ -1,2 +1,3 @@
 apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev
-sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
