66cb786c2d19d2dba96ade1839f0db39433c7611
simuns <sih@synack.fo>
2021-04-30 19:45:19 +0100

    correction

--- deb.sh
+++ deb.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/awesometic/realtek-r8125-dkms/releases/download/9.003.05-1/realtek-r8125-dkms_9.003.05-1_amd64.deb
+wget https://github.com/awesometic/realtek-r8125-dkms/archive/master.zip -O realtek-8125-dkms.zip
