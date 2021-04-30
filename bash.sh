026d6eca65a29e7e1fab0e5eeeaa1a842a283344
simuns <sih@synack.fo>
2021-04-30 19:26:45 +0100

    more dependencies

--- bash.sh
+++ bash.sh
@@ -1,1 +1,2 @@
 apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
