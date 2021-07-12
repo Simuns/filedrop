7baeff237fb6bc61ac79f90d581d743942ac30a8
simuns <sih@synack.fo>
2021-07-12 10:26:10 +0100

    included important comment

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev
 apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
 apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
-apt-get install linux-headers-4.19.0-16-amd64
+apt-get install linux-headers-4.19.0-17-amd64 #header version needs to match kernel version
 export PATH=$PATH:/sbin/
